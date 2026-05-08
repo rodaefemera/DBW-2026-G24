75b33b7d665ddceb5b1cb622a533ccd96a29a21a
Duarte <duarteferro76@gmail.com>
2026-05-08 13:44:24 +0100

    feat: convert HTML to EJS with partials, add routes and controllers for all pages

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
-const express = require('express');
+// Definição das rotas URL e métodos HTTP
+import express from 'express';
 const router = express.Router();
-const indexController = require('../controllers/indexController');
+import {
+    renderIndex,
+    renderLogin,
+    renderSignup,
+    renderProfile,
+    renderFriends,
+    renderRoomlist,
+    renderCreateroom,
+    renderRoombase,
+    renderLeaderboards,
+    renderFeedback,
+} from '../controllers/indexController.js';
 
-router.get('/', indexController.renderIndex);
+// Rotas GET para cada página
+router.get('/', renderIndex);
+router.get('/login', renderLogin);
+router.get('/signup', renderSignup);
+router.get('/profile', renderProfile);
+router.get('/friends', renderFriends);
+router.get('/roomlist', renderRoomlist);
+router.get('/createroom', renderCreateroom);
+router.get('/roombase', renderRoombase);
+router.get('/leaderboards', renderLeaderboards);
+router.get('/feedback', renderFeedback);
 
-module.exports = router;
+export default router;
