72a039d7fb0330cd8abfdf3a021b06e7c57de651
rodaefemera <2178524@student.uma.pt>
2026-05-16 14:15:14 +0100

    build: commit completo no pull, isto é só porque houve demasiadas colisões.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,19 +1,7 @@
-// Controller responsible for bridging data and views
+// Controller responsável por fazer a ponte entre os dados e as views temporáriamente até o back-end for criado
 
 function renderIndex(req, res) {
     res.render('index', { title: 'MATRIOSCA' });
-}
-
-function renderLogin(req, res) {
-    res.render('login', { title: 'Login - MATRIOSCA' });
-}
-
-function renderSignup(req, res) {
-    res.render('signup', { title: 'Sign Up - MATRIOSCA' });
-}
-
-function renderProfile(req, res) {
-    res.render('profile', { title: 'Profile - MATRIOSCA' });
 }
 
 function renderFriends(req, res) {
@@ -24,39 +12,18 @@ function renderRoomlist(req, res) {
     res.render('roomlist', { title: 'Rooms - MATRIOSCA' });
 }
 
-// Redirect to login if not authenticated
 function renderCreateroom(req, res) {
-    if (!req.isAuthenticated()) {
-        return res.redirect('/login');
-    }
     res.render('createroom', { title: 'Create Room - MATRIOSCA' });
 }
 
-// Redirect to login if not authenticated
 function renderRoombase(req, res) {
-    if (!req.isAuthenticated()) {
-        return res.redirect('/login');
-    }
     res.render('roombase', { title: 'Room - MATRIOSCA' });
-}
-
-function renderLeaderboards(req, res) {
-    res.render('leaderboards', { title: 'Leaderboard - MATRIOSCA' });
-}
-
-function renderFeedback(req, res) {
-    res.render('feedback', { title: 'Feedback - MATRIOSCA' });
 }
 
 module.exports = {
     renderIndex,
-    renderLogin,
-    renderSignup,
-    renderProfile,
     renderFriends,
     renderRoomlist,
     renderCreateroom,
     renderRoombase,
-    renderLeaderboards,
-    renderFeedback,
 };
