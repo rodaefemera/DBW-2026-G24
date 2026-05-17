efdd3c031e48d9d6467079a74ca603c453e042e7
Rungox7 <santiagovieira22@gmail.com>
2026-05-17 01:25:54 +0100

    Add files via upload

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -8,26 +8,7 @@ const renderFriends = (req, res) => {
     res.render('friends', { title: 'Friends - MATRIOSCA' });
 };
 
-const renderRoomlist = (req, res) => {
-    res.render('roomlist', { title: 'Rooms - MATRIOSCA' });
-};
-
-const renderCreateroom = (req, res) => {
-    if (!req.isAuthenticated()) return res.redirect('/login');
-    res.render('createroom', { title: 'Create Room - MATRIOSCA' });
-};
-
-const renderRoombase = (req, res) => {
-    if (!req.isAuthenticated()) return res.redirect('/login');
-    const mode     = req.query.mode     || 'time_attack';
-    const roomCode = req.query.roomCode || 'LOBBY';
-    res.render('roombase', { title: 'Room - MATRIOSCA', mode, roomCode });
-};
-
 module.exports = {
     renderIndex,
     renderFriends,
-    renderRoomlist,
-    renderCreateroom,
-    renderRoombase,
 };
